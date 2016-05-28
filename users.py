d99ae58c0616de8e74b3f8ca321597654dd3ce5e
Rentaro Matsukata <freefood89@users.noreply.github.com>
2016-05-27 23:56:29 -0400

    changed indentation

--- users.py
+++ users.py
@@ -5,39 +5,39 @@ from boto3.dynamodb.conditions import Key, Attr
 
 dynamodb = boto3.resource('dynamodb')
 
-def get_users_db(username):
+def get_users_from_db(username):
     table = dynamodb.Table('Users')
     response = table.query(
-    KeyConditionExpression=Key('username').eq(username)
-    )
-    for i in response['Items']:
-        return i
+        KeyConditionExpression=Key('username').eq(username)
+        )
+    return [user for user in response['Items']]
 
-def create_user_db(username,userType,publicInfo,protectedInfo,privateInfo):
+def create_user_in_db(username,userType,publicInfo,protectedInfo,privateInfo):
     table = dynamodb.Table('Users')
     response = table.put_item(
-    Item={
-        'username': usernmae,
-        'usertype': userType,
-        'publicInfo': publicInfo,
-        'protectedInfo': protectedInfo,
-        'privateInfo': privateInfo,
-
-        })
+        Item={
+            'username': usernmae,
+            'usertype': userType,
+            'publicInfo': publicInfo,
+            'protectedInfo': protectedInfo,
+            'privateInfo': privateInfo,
+            },
+        )
     return
 
-def update_user_db(username,userType,publicInfo,protectedInfo,privateInfo):
+def update_user_in_db(username,userType,publicInfo,protectedInfo,privateInfo):
     table = dynamodb.Table('Users')
     response = table.update_item(
-    Key={
-    'username': usernmae,
-    },
-    UpdateExpression="set info.rating = :r, info.plot=:p, info.actors=:a",
-    ExpressionAttributeValues={
-        ':usertype': userType,
-        ':publicInfo': publicInfo,
-        ':protectedInfo': protectedInfo,
-        ':privateInfo': privateInfo,
-    },
-    ReturnValues="UPDATED_NEW")
+        Key={
+            'username': usernmae,
+            },
+        UpdateExpression="set info.rating = :r, info.plot=:p, info.actors=:a",
+        ExpressionAttributeValues={
+            ':usertype': userType,
+            ':publicInfo': publicInfo,
+            ':protectedInfo': protectedInfo,
+            ':privateInfo': privateInfo,
+            },
+        ReturnValues="UPDATED_NEW",
+        )
     return
